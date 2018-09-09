91b0dca6cb4c3813f4c292f8ad34e1437ecd3ffe
Dylan <dpaiton@gmail.com>
2018-09-09 13:20:30 -0700

    reordered commands & added vim jupyter setup

--- extract_dotfiles.sh
+++ extract_dotfiles.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
-cp -r .jupyter/ ~/
-cd .jupyter/
+CWD=$PWD
+cp -fr .tmux/ ~/
+cp -f .tmux.conf ~/
+cp -f .bash* ~/
+cp -f .gitconfig ~/
+cp -f .profile ~/
+cp -fr scripts/ ~/
+cp -f .vimrc ~/
+cp -fr .vim ~/
+cp -fr .jupyter/ ~/
+chmod +x setup_jupyter_vim.sh
+cd ~/.jupyter/
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout mycert.pem -out mycert.pem
-cd ~/
-cp -r .tmux/ ~/
-cp .tmux.conf ~/
-cp .bash* ~/
-cp .gitconfig ~/
-cp .profile ~/
-cp -r scripts/ ~/
-cp .vimrc ~/
-cp -r .vim ~/
 mkdir -p ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cd $CWD
+./setup_jupyter_vim.sh
