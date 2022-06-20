b5b13f8c933b6065fa4744a19bb081750174a738
Dylan <dpaiton@gmail.com>
2022-06-20 15:00:57 -0700

    forces pulling container image from docker hub

--- docker/run-setup.sh
+++ docker/run-setup.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --gpus all --publish 8888:8888 --mount source=projects-volume,target=/projects --env JUPYTER_TOKEN=docker dpaiton/torch-jupyter:latest
+docker run --gpus all --publish 8888:8888 --mount source=projects-volume,target=/projects --env JUPYTER_TOKEN=docker --pull always dpaiton/torch-jupyter:latest
