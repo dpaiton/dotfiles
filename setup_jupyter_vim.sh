56ce0d47999b5d109564576605a436a9313f1640
Dylan <dpaiton@gmail.com>
2022-06-20 16:48:58 -0700

    typo fix on destination path for config

--- setup_jupyter_vim.sh
+++ setup_jupyter_vim.sh
@@ -8,7 +8,7 @@ cd $jupyter_dir/nbextensions
 git clone https://github.com/lambdalisue/jupyter-vim-binding vim_binding
 jupyter nbextension enable vim_binding/vim_binding
 
-cp -fr .jupyter/* $jupyter_dir
+cp -fr .jupyter/* $jupyter_dir/
 cd $jupyter_dir
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout mycert.key -out mycert.pem
 
